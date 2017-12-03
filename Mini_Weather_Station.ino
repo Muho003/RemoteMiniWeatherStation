10b3e6ba39586c285b618661623f8d179e749572
Muhotasim Ahmed <muho14@gmail.com>
2017-12-02 20:47:54 -0500

    Add files via upload

---
+++ Mini_Weather_Station.ino
@@ -0,0 +1,70 @@
+#include <LiquidCrystal.h>
+
+#include <VirtualWire.h>
+
+#include <DHT.h>
+
+#define DHTPIN 4
+
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+char msg[6];
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Temperature Sensor testing!");
+
+  dht.begin();   
+
+   vw_set_tx_pin(12);          // Sets pin D12 as the TX pin
+   vw_setup(2000);          // Bits per sec
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  dtostrf(t, 6, 2, msg); //converts the float to a char or string
+
+   vw_send((uint8_t *)msg, strlen(msg)); //transmits the data
+   vw_wait_tx(); // Wait until the whole message is gone
+   delay(200);
+
+   
+}
---
+++ Mini_Weather_Station_Receiver.ino
@@ -0,0 +1,22 @@
+
+#include <VirtualWire.h>
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+   Serial.begin(9600);
+    
+    // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+    // Print a message to the LCD.
+  lcd.print("hello, world!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
